42a116ce79da28702b4f1c47536e94d603f4019e
wiktoriya melnikova <ebayebay56@gmail.com>
2023-03-16 19:27:52 +0300

    slick add

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 300,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/right.svg"></button>',
+        responsive: [
+            {  
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                }
+            },
+
+            
+        ]
+      });
+  });
+ 
